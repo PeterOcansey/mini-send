b860f55eb3adc7f9e4840393c9966754aaba12cb
peter <ocanseypeter@gmail.com>
2022-07-27 06:02:36 +0000

    Add a test to check if email can be sent

--- backend/tests/Feature/HttpTest.php
+++ backend/tests/Feature/HttpTest.php
@@ -21,28 +21,39 @@ class HttpTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        //EmailTransaction::factory()->count(10)->create();
+        EmailTransaction::factory()->count(10)->create();
 
         $response = $this->get("/api/emails");
         $response->assertStatus(200);
 
         $response->assertJsonStructure( ['code','message', 'data'] );
+    }
 
-        // $response->assertJsonStructure(
-        //     [
-        //         'data' => [
-        //                 'id',
-        //                 'uid',
-        //                 'from',
-        //                 'to',
-        //                 'subject',
-        //                 'content_text',
-        //                 'content_html',
-        //                 'status',
-        //                 'created_at',
-        //                 'updated_at'
-        //         ]
-        //     ]
-        // );
+    /**
+     * Test sending email transactions endpoint
+     * Assert our endpoint is ok
+     * Assert our endpoint returns a valid json strucutre
+     *  
+     * @test 
+     * */
+    public function email_can_be_sent()
+    {
+        $this->withoutExceptionHandling();
+
+        $data = [
+            'uid' => "77u7u88yt3490ur",
+            'from' => "test@minisend.com",
+            'to' => "test@minisend.com",
+            'subject' => "Hello test",
+            'content_text' => "Hello test content"
+        ];
+
+        $response = $this->post( "/api/emails", $data );
+        $response->assertStatus( 200 );
+        $response->assertJsonStructure( ['code','message', 'data'] );
+        $response->assertJson( ['message' => "Product Created!"] );
+        $response->assertJson( ['data' => ['data' => $data ]] );
+
+        
     }
 }
