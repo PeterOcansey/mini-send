57f8564f7ccc6e9be7e786a27ebe3e5ab740be6a
peter <ocanseypeter@gmail.com>
2022-07-28 01:01:47 +0000

    Renamed DashboardView to TransactionsView

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import DashboardView from '../views/DashboardView.vue';
+import TransactionsView from '../views/TransactionsView.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'dashboard',
-    component: DashboardView,
+    name: 'transactions',
+    component: TransactionsView,
   },
 ];
 
